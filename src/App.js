d3fd77c78d5f11a568b1832b1e34fa7132e188ac
Bhargavi Chanaboina <bhargavi.ch17@gmail.com>
2025-11-18 17:15:58 +0530

    base code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const apiKey = 'ad73b65e098a4caf7d846a8efaf6086c';
+      const city = 'Hyderabad';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setWeather(data);
+      console.log(data);
+      setLoading(false);
+    }
+
+    fetchWeather();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather in {weather.name}</h1>
+      <p>Temperature: {weather.main.temp}°C</p>
+      <p>Description: {weather.weather[0].description}</p>
     </div>
   );
 }
