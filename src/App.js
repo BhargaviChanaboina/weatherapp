c1023676dc039346b0c879851c9a000ac70417c1
Bhargavi Chanaboina <bhargavi.ch17@gmail.com>
2025-11-22 13:16:22 +0530

    Full functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,16 @@ function App() {
   const [city, setCity] = useState('');
   const [unhandledError, setUnhandledError] = useState(null);
 
+  const handleSearch = () => {
+
+    if (!tempCity.trim()) {
+      alert('Please enter a city name');
+      return;
+    }
+    setCity(tempCity);
+    setTempCity('');
+
+  }
   useEffect(() => {
     const fetchWeather = async () => {
       try {
@@ -22,7 +32,7 @@ function App() {
 
           setLoading(true);
 
-          const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.trim()}&appid=${apiKey}&units=metric`;
           const response = await fetch(url);
           const data = await response.json();
           setWeather(data);
@@ -47,33 +57,46 @@ function App() {
     <div>
       <header className="App-header">
         <h1>Weather Tracker&nbsp;&nbsp;
-          <i className="fa fa-check-square"></i>
         </h1>
+       
       </header>
       <div style={{padding: '15px', backgroundColor: 'lightblue', marginBlockStart: '40px', width: '60%',
            alignContent: 'center', marginLeft: 'auto', marginRight: 'auto', borderRadius: '12px'}}>
 
-      <input type='text' onKeyPress = {(e) => {if (e.key === 'Enter') setCity(tempCity)}} 
-        onChange={(e) => setTempCity(e.target.value)}
+      <input type='text' value={tempCity} onKeyPress = {(e) => {if (e.key === 'Enter') setCity(tempCity)}} 
+        onChange={(e) => setTempCity(e.target.value)
+        }
         style={{height:'40px', borderRadius: '8px', width: '250px' }} ></input>
 
         &nbsp;&nbsp;&nbsp;
       <button style={{height: '40px', backgroundColor: 'limegreen', borderRadius: '16px', cursor: "pointer"}}
-      onClick={() => setCity(tempCity)}>Get weather</button>
+      onClick={handleSearch}>Get weather</button>
+      
 
       {!city && <div>No data</div> }
 
       {loading && <div>Loading...</div>}
 
-      {!loading && weather && weather.cod == 200 && (
+      {!loading && weather && weather.cod === 200 && (
         <div>
           <h1>Weather in {weather.name}</h1>
-          <p>Temperature: {weather.main.temp}°C</p>
+          <p style={{display: 'flex', alignItems: 'center', gap: '6px'}}>Temperature: {weather.main.temp}°C
+            <img 
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt="weather"
+                style={{width:'30px', height: '30px'}}
+              />
+          </p>
           <p>Description: {weather.weather[0].description}</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+          <p>Pressure: {weather.main.pressure}hPa</p>
+          <p>Maximum temperature: {weather.main.temp_max}°C</p>
+          <p>Minimum temperature: {weather.main.temp_min}°C</p>
+          <p>Wind speed: {weather.wind.speed}m/sec</p>
         </div>) 
       }
-
-      {!loading && weather && weather.cod != 200 && <h5>Message: {weather.message}</h5>}
+      {!loading && weather && weather.cod === '404' && <div><h3>City not found!</h3></div>}
+      {!loading && weather && weather.cod !== 200 && weather.cod !== '404' && <h5>Message: {weather.message}</h5>}
 
       { unhandledError && <div>Error: {unhandledError}</div> }
     </div>
